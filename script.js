be26b955cf123b902d1f3cb5a68a9720dd3c7fd9
Gahbx <138726167+Gahbx@users.noreply.github.com>
2024-10-08 00:40:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const  ul= document.querySelector('ul')
+const novaTarefa = document.getElementById('novaTrf');
+
+function adicionarTarefa(){
+  //valida a tarefa 
+
+
+   if(novaTarefa.value == ''){
+    return
+   };
+   const barraTrf= document.createElement('div');
+   barraTrf.classList.add('trfs')
+
+
+ //check
+   const check = document.createElement('input');
+     check.type= 'checkbox';
+     check.classList.add('check')
+     
+// li 
+     const li= document.createElement('li');
+     li.textContent = novaTarefa.value  
+//edit
+     
+    const btnEdit= document.createElement('button');
+    btnEdit.classList.add('btnEdit')
+    btnEdit.addEventListener('click', ()=>{ 
+       
+    const trfEdit= document.createElement('input');
+    li.textContent = prompt('Edite a Tarefa') ;
+
+   });
+   
+//remove 
+   const btnRemove= document.createElement('button');
+   btnRemove.classList.add('remover')
+   btnRemove.addEventListener('click',()=>{
+    barraTrf.remove()
+   } );
+
+  
+     barraTrf.appendChild(btnRemove)
+     barraTrf.appendChild(btnEdit)
+     barraTrf.appendChild(li)
+     barraTrf.prepend(check)
+     ul.appendChild(barraTrf)
+
+};
